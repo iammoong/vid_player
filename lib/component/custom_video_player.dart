142c50d62d4306401e42aab520f44373526e57e5
iammoong <ansrlwkd225@naver.com>
2024-02-29 22:57:59 +0900

    비디오 플레이어 플레이버튼, 뒤로가기 버튼, 앞으로가기 버튼, 목록으로 돌아가기 버튼 만듬

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -40,6 +40,129 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     if(videoController == null) {
       return CircularProgressIndicator();
     }
-    return VideoPlayer(videoController!);
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+              videoController!,
+          ),
+          _Controlls(
+            onReversePressed: onReversePressed,
+            onPlayPressed: onPlayPressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoController!.value.isPlaying,
+          ),
+          Positioned(
+            right: 0,
+            child: IconButton(
+                onPressed: (){},
+                color: Colors.white,
+                iconSize: 30.0,
+                icon: Icon(
+              Icons.photo_camera_back
+            )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onReversePressed() {
+    // 영상의 현재 포지션
+    final currentPosition = videoController!.value.position;
+
+    // 영상 처음 부분(0초)를 나타냄
+    Duration position = Duration();
+
+    if(currentPosition.inSeconds > 3) {
+      position = currentPosition - Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+
+  }
+
+  void onForwardPressed(){
+    // 영상의 전체 길이를 나타냄
+    final maxPosition = videoController!.value.duration;
+    final currentPosition = videoController!.value.position;
+    Duration position = maxPosition;
+
+    if((maxPosition -Duration(seconds: 3)).inSeconds > currentPosition.inSeconds) {
+      position = currentPosition + Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    
+    // 실행중인지 아닌지 값을 받아오지만 setState를 통해 다시 빌드를 해야함
+    setState(() {
+      
+    });
+    // 이미 실행중이면 중지
+    if(videoController!.value.isPlaying){
+      videoController!.pause();
+    } else { // 실행 중이 아니면 재생
+      videoController!.play();
+    }
+  } // onPlayPressed()
+}
+
+class _Controlls extends StatelessWidget {
+
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controlls({
+    required this.onPlayPressed,
+    required this.onReversePressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
+    Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+}) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        iconData,
+      ),
+    );
   }
 }
